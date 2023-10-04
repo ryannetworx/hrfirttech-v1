2c1ef1fd1680c2e9baf347e1e63b58dcebc14a54
ryannetworx <ryannetworx@gmail.com>
2023-10-04 19:23:47 +0800

    transfer from desktop

---
+++ script.js
@@ -0,0 +1,26 @@
+// Hero Images Container
+document.addEventListener("DOMContentLoaded", function () {
+    const imageContainers = document.querySelectorAll(".image-container");
+    let currentIndex = 0;
+
+    function showImage(index) {
+        imageContainers.forEach((container, i) => {
+            if (i === index) {
+                container.classList.add("active");
+            } else {
+                container.classList.remove("active");
+            }
+        });
+    }
+
+    function nextImage() {
+        currentIndex = (currentIndex + 1) % imageContainers.length;
+        showImage(currentIndex);
+    }
+
+    // Initially, show the first image
+    showImage(currentIndex);
+
+    // Automatically change images every 5 seconds (adjust as needed)
+    setInterval(nextImage, 5000);
+});
